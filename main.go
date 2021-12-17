87194eba307e92eb559f093c7e85c1f1a166fc7b
Leonardo Magdanello <lmagdanello40@gmail.com>
2021-12-17 17:16:34 -0300

    Print the file/dir permissions (0777, 0664, ...)

--- main.go
+++ main.go
@@ -12,7 +12,8 @@ import (
 
 func wif(target []string) string {
 
-	var answer string
+	var perm string
+	var file string
 	green := ansi.ColorFunc("green+")
 	red := ansi.ColorFunc("red+")
 	blue := ansi.ColorFunc("blue+")
@@ -23,48 +24,37 @@ func wif(target []string) string {
 	}
 
 	fi, err := os.Lstat(target[1])
-
 	if err != nil {
 		log.Fatal(err)
+		os.Exit(1)
+	}
+
+	perm = fmt.Sprintf("%#o", fi.Mode().Perm())
+	file, err = filepath.Abs(fi.Name())
+	if err != nil {
+		fmt.Println("Error")
+		os.Exit(1)
 	}
 
 	switch mode := fi.Mode(); {
 	case mode.IsRegular():
-		answer, err = filepath.Abs(fi.Name())
-		if err != nil {
-			fmt.Println("Error")
-			os.Exit(1)
-		}
-
-		return filepath.Join("Regular file: " + green(answer))
+		return filepath.Join("Regular file: "+green(file), "\nPerm: "+green(perm))
 
 	case mode.IsDir():
-		answer, err = filepath.Abs(fi.Name())
-		if err != nil {
-			fmt.Println("Error")
-			os.Exit(1)
-		}
-
-		return filepath.Join("Directory: " + red(answer))
+		return filepath.Join("Directory: "+red(file), "\nPerm: "+red(perm))
 
 	case mode&fs.ModeSymlink != 0:
-		answer, err = filepath.Abs(fi.Name())
-		if err != nil {
-			fmt.Println("Error")
-			os.Exit(1)
-		}
-
-		return filepath.Join("Symbolic link: " + blue(answer))
+		return filepath.Join("Symbolic link: "+blue(file), "\nPerm: "+blue(perm))
 	}
 
-	return answer
+	return file
 }
 
 func main() {
 
 	if len(os.Args) > 1 {
-		answer := wif(os.Args)
-		fmt.Println(answer)
+		file := wif(os.Args)
+		fmt.Println(file)
 	} else {
 		fmt.Println("No arguments were specified!")
 		os.Exit(1)
